32627cd5e705b0f32486cce56034bf124ec513ad
Julius Kalume <juliuskalume906@gmail.com>
2026-02-03 22:04:47 +0300

    Replace deprecated toolbar color API

--- android-pwa/app/src/main/java/com/fullcalculus/app/LauncherActivity.java
+++ android-pwa/app/src/main/java/com/fullcalculus/app/LauncherActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.browser.customtabs.CustomTabColorSchemeParams;
 import androidx.core.content.ContextCompat;
 
 public class LauncherActivity extends Activity {
@@ -11,8 +12,11 @@ public class LauncherActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     Uri uri = Uri.parse(getString(R.string.launch_url));
+    int toolbarColor = ContextCompat.getColor(this, R.color.colorPrimary);
+    CustomTabColorSchemeParams colorParams =
+        new CustomTabColorSchemeParams.Builder().setToolbarColor(toolbarColor).build();
     CustomTabsIntent intent = new CustomTabsIntent.Builder()
-        .setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
+        .setDefaultColorSchemeParams(colorParams)
         .build();
     intent.launchUrl(this, uri);
     finish();
