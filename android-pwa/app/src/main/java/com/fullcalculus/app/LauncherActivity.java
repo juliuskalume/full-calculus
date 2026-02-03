b4ad7ad9a89e123047cb3eceea29505900e0ac23
Julius Kalume <juliuskalume906@gmail.com>
2026-02-03 21:46:39 +0300

    Use CustomTabsIntent builder

--- android-pwa/app/src/main/java/com/fullcalculus/app/LauncherActivity.java
+++ android-pwa/app/src/main/java/com/fullcalculus/app/LauncherActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.browser.customtabs.CustomTabsIntent;
-import androidx.browser.trusted.TrustedWebActivityIntentBuilder;
 import androidx.core.content.ContextCompat;
 
 public class LauncherActivity extends Activity {
@@ -12,9 +11,9 @@ public class LauncherActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     Uri uri = Uri.parse(getString(R.string.launch_url));
-    TrustedWebActivityIntentBuilder builder = new TrustedWebActivityIntentBuilder(uri)
-        .setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
-    CustomTabsIntent intent = builder.build();
+    CustomTabsIntent intent = new CustomTabsIntent.Builder()
+        .setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
+        .build();
     intent.launchUrl(this, uri);
     finish();
   }
