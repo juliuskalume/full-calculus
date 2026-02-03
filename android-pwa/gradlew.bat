78e43204c1f58d75ba586fa6cbb34871d9ffb2b8
Julius Kalume <juliuskalume906@gmail.com>
2026-02-04 01:04:32 +0300

    Fix gradle wrapper classpath for shared jar

--- android-pwa/gradlew.bat
+++ android-pwa/gradlew.bat
@@ -1,5 +1,5 @@
 @echo off
 set DIR=%~dp0
 set APP_HOME=%DIR%
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar;%APP_HOME%\gradle\wrapper\gradle-wrapper-shared.jar
 "%JAVA_HOME%\bin\java" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
